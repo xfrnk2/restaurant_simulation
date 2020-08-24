e010a494220c48ea5a946936d689eacf9343377c
xfrnk2 <xfrnk2@gmail.com>
2020-08-24 22:00:57 +0900

    총괄 클래스의 손님 응대 관련 일부 함수와 대기열의 상태를 갱신하는 함수를 구현

--- src/restaurant.py
+++ src/restaurant.py
@@ -25,6 +25,41 @@ class Restaurant:
         self.__food_eating_time = {1: 30, 2: 20, 3: 15, 4: 10}
         self.__food_cooking_time = {1: 30, 2: 20, 3: 10, 4: 15}
 
+    def customer_visiting(self, elapsed_time: int):
+        self.__number_of_customers += 1
+        new_customer = Customer(self.__number_of_customers, elapsed_time)
+
+        print(f"{self.__number_of_customers}번째 손님이 시각 {elapsed_time} 분에 레스토랑에 도착했습니다.")
+        return new_customer
+
+    def receive_customer(self, customer: Customer):
+        self.__waiting_customers.append(customer)
+
+    def pop_waiting_queue(self, pop_count: int):
+
+        if pop_count != 0:
+            for _ in range(pop_count):
+                self.__waiting_customers.pop(0)
+
+    def waiting_update(self):
+        if self.__waiting_customers:
+            customer_count = 0
+
+            for customer in self.__waiting_customers:
+                customer.waiting_update()
+
+            for customer in self.__waiting_customers:
+
+                if customer.get_elapsed_waiting_time() < customer.get_remaining_time_by_new_table():
+                    break
+
+                if customer.get_elapsed_waiting_time() == customer.get_remaining_time_by_new_table() and not self.__table_manager.is_table_full():
+                    self.customer_entrance(customer)
+                    customer_count += 1
+
+            self.pop_waiting_queue(customer_count)
+
+
     def run(self):
 
         elapsed_time = 0
