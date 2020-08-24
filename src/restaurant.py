f8e5625eb52e5f6c3c48838ae33c124ea4c64a02
xfrnk2 <xfrnk2@gmail.com>
2020-08-24 11:44:25 +0900

    전체 총괄 역할의 클래스 restaurant  정의

---
+++ src/restaurant.py
@@ -0,0 +1,7 @@
+class Restaurant:
+
+    def __init__(self, customer_visiting_period : int):
+        billing_period = 5
+        cash_desk_num = 1
+        cooks_num = 3
+        table_quantity = 20
