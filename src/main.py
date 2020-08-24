6dd98ac8dfb507b035156d35f2128d6337cd7f79
xfrnk2 <xfrnk2@gmail.com>
2020-08-24 11:46:55 +0900

    총괄 클래스 restaurant의 tick 발생 기능 구현, main.py 파일 추가

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from restaurant import Restaurant
+
+if __name__ == "__main__":
+    visiting_period = 5
+
+    restaurant = Restaurant(visiting_period)
+    restaurant.run()
--- src/restaurant.py
+++ src/restaurant.py
@@ -5,3 +5,11 @@ class Restaurant:
         cash_desk_num = 1
         cooks_num = 3
         table_quantity = 20
+
+    def run(self):
+
+        elapsed_time = 0
+
+        while elapsed_time < 720:
+
+            elapsed_time += 1
