32230aeb45adc74d6e1ba5aed0f17ff8ee0103d4
xfrnk2 <xfrnk2@gmail.com>
2020-09-13 01:39:46 +0900

    가독성을 위해 dataclass의 아이템을 나누지 않고 그대로 사용하도록 변경, getter 기능을하는 property 데코레이터를 붙인 함수를 작성, 기존함수 제거

--- src/cook.py
+++ src/cook.py
@@ -1,43 +1,35 @@
 from restaurant_object import RestaurantObject
-from dataclasses import dataclass
+from dataclasses import dataclass, replace
 
 class Cook(RestaurantObject):
 
     def __init__(self):
-        self.__food_number = 0
-        self.__cooking_time = 0
-        self.__customer_number = 0
-        self.__table_number = 0
+        self.__info = None
         self.__is_cooking = False
 
     def set_request(self, request: dataclass):
-        self.__table_number, self.__customer_number, self.__food_number, self.__cooking_time =\
-        request.table_num, request.customer_num,  request.food_num, request.cooking_time
-
-
+        food_info = request
+        self.__info = food_info
         self.__is_cooking = not self.__is_cooking
 
     def get_order_info(self) -> tuple:
-        return self.__table_number, self.__customer_number,  self.__food_number
+        return self.__info.table_num, self.__info.customer_num, self.__info.food_num
 
     def reset_status(self):
-        self.__table_number, self.__food_number, \
-             self.__customer_number, self.__cooking_time = 0, 0, 0, 0
+        self.__info.items = 0, 0, 0, 0
 
-    def is_cooking(self) -> bool:
+    @property
+    def is_cooking(self):
         return self.__is_cooking
 
-    def set_is_cooking(self):
-        self.__is_cooking = not self.__is_cooking
-
     def get_left_cooking_time(self) -> int:
-        return self.__cooking_time
+        return self.__info.cooking_time
 
     def update(self) -> bool:
 
         if self.__is_cooking:
-            self.__cooking_time -= 1
-            if self.__cooking_time == 0:
+            self.__info.cooking_time -= 1
+            if self.__info.cooking_time == 0:
                 self.__is_cooking = not self.__is_cooking
                 return True
         return False
