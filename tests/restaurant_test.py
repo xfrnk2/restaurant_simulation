5502b06ae6c92ec08dfb128cf1f49473d400b4fd
xfrnk2 <xfrnk2@gmail.com>
2020-09-28 20:08:08 +0900

    coveralls 임시 테스트 케이스 추가

--- tests/restaurant_test.py
+++ tests/restaurant_test.py
@@ -1,5 +1,11 @@
 from src.restaurant import Restaurant
-
+from src.random import randrange
+from src.customer import Customer
+from src.table import TableManager
+from src.kitchen import Kitchen
+from src.bill import BillManager
+from random import randrange
+from dataclasses import dataclass
 
 class TestClass(object):
 
