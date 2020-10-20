9aacb405b4f3de2f066585809f6498da1544934a
xfrnk2 <xfrnk2@gmail.com>
2020-10-20 13:02:36 +0900

    간단한 테스트 코드 작성

---
+++ tests/restaurant_test.py
@@ -0,0 +1,10 @@
+from src.restaurant import Restaurant
+from src.customer import Customer
+
+visiting_period = 5
+restaurant = Restaurant(visiting_period)
+
+def test_customer_visiting():
+    customer = restaurant.customer_visiting(0)
+    assert isinstance(customer, Customer), "리턴값이 Customer 클래스의 인스턴스가 아님"
+    assert customer.info.number % visiting_period == 0, "방문 주기에 맞게 손님이 방문하지 않음"
