662b92b000cc09ac727d46003a90b3fe4acc1502
xfrnk2 <xfrnk2@gmail.com>
2020-10-24 22:22:39 +0900

    기존의 테스트 내용 일부 복구

--- tests/restaurant_test.py
+++ tests/restaurant_test.py
@@ -4,3 +4,6 @@ from restaurant_simulation.src.customer import Customer
 visiting_period = 5
 restaurant = Restaurant(visiting_period)
 
+def test_customer_visiting():
+    customer = restaurant.customer_visiting(0)
+    assert isinstance(customer, Customer), "리턴값이 Customer 클래스의 인스턴스가 아님"
