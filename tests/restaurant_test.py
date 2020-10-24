86094c1bf9ec1906317aa1cfee68111335151dab
xfrnk2 <xfrnk2@gmail.com>
2020-10-25 01:46:42 +0900

    손님의 정보를 초기화하는 테스트 코드 작성, 코드가 실패한다.

--- tests/restaurant_test.py
+++ tests/restaurant_test.py
@@ -1,3 +1,24 @@
-def test_restaurant():
-    a = 1
-    assert a == 1, "test"
+from collections import namedtuple
+from src.restaurant import customer_initialize
+
+Case = namedtuple("TestCase", "customer_num food_num expected")
+CustomerInfo = namedtuple("CustomerInfo", "customer_num food_num eating_time cooking_time")
+
+def test_customer_initialize():
+    cases = (
+        Case(customer_num=3,
+             food_num=1,
+             expected=(3, 1, 30, 30, "스테이크")
+             ),
+        Case(customer_num=1,
+             food_num=2,
+             expected=(1, 2, 20, 20, "스파게티")
+             ),
+        Case(customer_num=3,
+             food_num=4,
+             expected=(3, 4, 10, 15, "그라탱")
+             ),
+    )
+    for case in cases:
+        customer_num, food_num, expected = case
+        assert customer_initialize(customer_num, food_num)  == expected
