0968e9a7e6e866fba166211c5f8121cb39797018
xfrnk2 <xfrnk2@gmail.com>
2020-11-01 23:59:33 +0900

    새로운 테스트 케이스를 생성. 아직 테스트 케이스가 실패한다.

--- tests/restaurant_test.py
+++ tests/restaurant_test.py
@@ -1,24 +1,37 @@
 from collections import namedtuple
-from src.restaurant import customer_initialize
+from src.restaurant import customer_initialize, waitable
 
-Case = namedtuple("TestCase", "customer_num food_num expected")
-CustomerInfo = namedtuple("CustomerInfo", "customer_num food_num eating_time cooking_time")
+InitializeCase = namedtuple("InitializeTestCase", "customer_num food_num expected")
+WaitableCase = namedtuple("WaitableTestCase", "not_taken_order taken_order expected")
 
 def test_customer_initialize():
     cases = (
-        Case(customer_num=3,
-             food_num=1,
-             expected=(3, 1, 30, 30, "스테이크")
-             ),
-        Case(customer_num=1,
-             food_num=2,
-             expected=(1, 2, 20, 20, "스파게티")
-             ),
-        Case(customer_num=3,
-             food_num=4,
-             expected=(3, 4, 10, 15, "그라탱")
-             ),
+        InitializeCase(customer_num=3,
+                       food_num=1,
+                       expected=(3, 1, 30, 30, "스테이크")
+                       ),
+        InitializeCase(customer_num=1,
+                       food_num=2,
+                       expected=(1, 2, 20, 20, "스파게티")
+                       ),
+        InitializeCase(customer_num=3,
+                       food_num=4,
+                       expected=(3, 4, 10, 15, "그라탱")
+                       ),
     )
     for case in cases:
         customer_num, food_num, expected = case
         assert customer_initialize(customer_num, food_num)  == expected
+
+def test_waitable():
+
+    cases = (
+        WaitableCase(not_taken_order=[5, 10],
+                     taken_order=[5, 10, 10],
+                     expected=10
+                     )
+    )
+
+    for case in cases:
+        not_taken_order, taken_order, expected = case
+        assert waitable(not_taken_order, taken_order) == expected
