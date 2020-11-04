1d3589088125c0862801c636fc6306c978085b1a
xfrnk2 <xfrnk2@gmail.com>
2020-11-05 00:01:47 +0900

    직전 커밋에서 구현하고자 했던 로직을 대체하는, 새로운 로직에 맞는 테스트 케이스를 작성

--- tests/restaurant_test.py
+++ tests/restaurant_test.py
@@ -1,8 +1,8 @@
 from collections import namedtuple
-from src.restaurant import customer_initialize, calc_remaining_time_part1
+from src.restaurant import customer_initialize, waiting_checker
 
 InitializeCase = namedtuple("InitializeTestCase", "customer_num food_num expected")
-calcTimePart1Case = namedtuple("calcTimePart1Case", "not_taken_order taken_order expected")
+WaitingCase = namedtuple("WaitingCase", "tables waiting_amount waitable_time expected")
 
 def test_customer_initialize():
     cases = (
@@ -23,22 +23,40 @@ def test_customer_initialize():
         customer_num, food_num, expected = case
         assert customer_initialize(customer_num, food_num)  == expected
 
-def test_calc_remaining_time_part1():
+def test_waiting_checker():
 
     cases = (
-        calcTimePart1Case(not_taken_order=[5, 10],
-                          taken_order=[5, 10, 10],
-                          expected=10
-                          ),
-        calcTimePart1Case(not_taken_order=[7, 9],
-                          taken_order=[5, 10, 15],
-                          expected=12
-                          ),
-        calcTimePart1Case(not_taken_order=[],
-                          taken_order=[5, 10, 15],
-                          expected=5
-                          )
+        WaitingCase(tables=[10, 20, 30],
+                    waiting_amount=2,
+                    waitable_time=15,
+                    expected=False
+                     ),
+        WaitingCase(tables=[15, 20, 25, 30, 35],
+                    waiting_amount=1,
+                    waitable_time=20,
+                    expected=True
+                    ),
+        WaitingCase(tables=[15, 21, 23, 30, 37, 40, 45],
+                    waiting_amount=1,
+                    waitable_time=41,
+                    expected=True
+                    ),
+        WaitingCase(tables=[15, 21, 23, 30, 37, 40, 45],
+                    waiting_amount=0,
+                    waitable_time=41,
+                    expected=True
+                    ),
+        WaitingCase(tables=[21, 23, 30, 37, 40, 45],
+                    waiting_amount=0,
+                    waitable_time=15,
+                    expected=False
+                    ),
+        WaitingCase(tables=[15, 21, 23, 30, 37, 40, 45],
+                    waiting_amount=3,
+                    waitable_time=21,
+                    expected=False
+                    ),
     )
     for case in cases:
-        taken_order, not_taken_order, expected = case
-        assert calc_remaining_time_part1(taken_order, not_taken_order) == expected
+        tables, waiting_amount, waitable_time, expected = case
+        assert waiting_checker(tables, waiting_amount, waitable_time) == expected
