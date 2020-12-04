6ec1d6bbbbb7aca91918d0b87e2de0888dbf2d15
xfrnk2 <xfrnk2@gmail.com>
2020-12-04 18:52:17 +0900

    중간 커밋

--- src/restaurant_2.py
+++ src/restaurant_2.py
@@ -1,10 +1,15 @@
+from collections import namedtuple
+
+CustomerInfo = namedtuple('CustomerInfo', 'number, table, food')
+
+
 
 
 class Customer:
 
-    def __init__(self, number, table, food):
-        self.__info = (number, table, food)
-        self.__time = 0
+    def __init__(self, number, table, food, time):
+        self.__info = CustomerInfo(number = number, table = table, food = food)
+        self.__time = time
         self.__eating = False
 
     @property
@@ -12,8 +17,12 @@ class Customer:
         return self.__info
 
     @property
-    def complete(self):
+    def dish(self):
         return self.__eating
+
+    @property
+    def time(self):
+        return self.__time
 
     def __update(self):
         self.__time -= 1
@@ -23,21 +32,31 @@ class Customer:
         self.__time = time
         self.__eating = not self.__eating
 
+
 class Table:
 
     def __init__(self, amount):
         self.table = {i: j for i, j in zip(range(1, amount + 1), [None] * amount)}
         self.__cooking_time = {1: 30, 2: 20, 3: 10, 4: 15}
         self.__eating_time = {1: 30, 2: 20, 3: 15, 4: 10}
-        self.__cook = Cook()
+        self.__cook = []
 
     def __update(self):
-        for n in self.table:
-            if n.__update():
-                if n.complete:
-                    #밖으로 이동
-                    return
-                n.__change_status(self.__eating_time)
+        finish = []
+
+        for i in self.table.keys():
+            if not isinstance(self.table[i], Customer):
+                continue
+
+            if self.table[i].__update():
+                if not self.table[i].dish:
+                    time = self.__eating_time[self.table[i].info.food]
+                    self.table[i].__change_status(time)
+                    continue
+
+                finish.append(self.table[i].info.number)
+
+        return finish
 
     def empty(self):
         empty = []
@@ -46,12 +65,41 @@ class Table:
                 empty.append(key)
         return empty
 
-class Cook:
-    def __init__(self):
-        self.cook = []
 
-    def __update(self):
-        pass
+class LogicTable:
+    def __init__(self, amount):
+        self.__table = Table(amount)
+        self.__current_waitable_time = 0
+        self.__cooking_time = {1: 30, 2: 20, 3: 10, 4: 15}
+        self.__eating_time = {1: 30, 2: 20, 3: 15, 4: 10}
+
+    @property
+    def table(self):
+        return self.__table
+
+    @property
+    def waitable_time(self):
+        return self.__current_waitable_time
+
+    def is_waitable(self, waitable_time, waiting_amount):
+
+        available = [customer.time for customer in self.__table.table.values() if customer.time <= waitable_time]
+        if waiting_amount - len(available) < 0:
+            return True
+
+        if waiting_amount - len(available) < len(self.__table.table):
+            self.__current_waitable_time = available[waiting_amount].time
+        else:
+            self.__current_waitable_time = available[-1].time
+        return False
+
+
+
+
+
+
+
+
 
 class Bill:
     def __init__(self):
@@ -67,4 +115,3 @@ class Bill:
             self.__time = 5
             self.__waiting.pop(0)
             #문구
-
